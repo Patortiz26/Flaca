304a492375e7338f67ed3a0279514d1c60dfe5b8
Patortiz26 <89476610+Patortiz26@users.noreply.github.com>
2024-04-05 19:12:47 -0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -5,6 +5,17 @@ from reportlab.lib.pagesizes import letter, landscape
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from io import BytesIO
 
+cols_tt = ['Tipo Compra', 'RUT Proveedor', 'Razon Social', 'Folio',
+       'Fecha Docto', 'Fecha Recepcion', 'Fecha Acuse', 'Monto Exento',
+       'Monto Neto', 'Monto IVA Recuperable', 'Monto Iva No Recuperable',
+       'Codigo IVA No Rec.', 'Monto Total', 'Monto Neto Activo Fijo',
+       'IVA Activo Fijo', 'IVA uso Comun', 'Impto. Sin Derecho a Credito',
+       'IVA No Retenido', 'Tabacos Puros', 'Tabacos Cigarrillos',
+       'Tabacos Elaborados', 'NCE o NDE sobre Fact. de Compra',
+       'Codigo Otro Impuesto', 'Valor Otro Impuesto', 'Tasa Otro Impuesto', '']
+columnas_a_mantener = ['Tipo Compra', 'RUT Proveedor', 'Razon Social', 'Folio', 'Fecha Docto', 'Monto Neto', 'Monto IVA Recuperable', 'Monto Total']
+
+
 def df_to_pdf(df):
     # Crear un objeto BytesIO para el PDF
     pdf_buffer = BytesIO()
@@ -19,8 +30,8 @@ def df_to_pdf(df):
     table_style = TableStyle([
         ('ALIGN', (0,0), (-1,-1), 'CENTER'),
         ('VALIGN', (0,0), (-1,-1), 'MIDDLE'),
-        ('INNERGRID', (0,0), (-1,-1), 0.5, colors.black),
-        ('BOX', (0,0), (-1,-1), 0.5, colors.black),
+        ('INNERGRID', (0,0), (-1,-1), 0.5, colors.grey),
+        ('BOX', (0,0), (-1,-1), 0.5, colors.grey),
         ('BACKGROUND', (0,0), (-1,0), colors.grey),
         ('FONTSIZE', (0,0), (-1,-1), 8),  # Establecer el tamaño de la fuente aquí
     ])
@@ -38,7 +49,7 @@ st.write('Programa para el uso exclusivo de Sandra Vargas')
 st.write('Desarrollado por Patricio Ortiz: ptricio.ortiz.v@ug.uchile.cl')
 
 archivo_cargado = st.file_uploader("Sube un archivo Excel o CSV", type=['xlsx', 'csv'])
-nombre_archivo_salida = st.text_input("Nombre del archivo de salida (sin extensión)", "")
+nombre_archivo_salida = st.text_input("Nombre del archivo de salida (sin extensión)", "datos_filtrados")
 
 if archivo_cargado is not None:
     if archivo_cargado.name.endswith('.xlsx'):
@@ -46,10 +57,13 @@ if archivo_cargado is not None:
     elif archivo_cargado.name.endswith('.csv'):
         df = pd.read_csv(archivo_cargado, sep=';')
     
-    columnas_a_mantener = ['Tipo Compra', 'RUT Proveedor', 'Razon Social', 'Folio', 'Fecha Docto', 'Monto Neto', 'Monto IVA Recuperable', 'Monto Total']
+    
     
     if all(columna in df.columns for columna in columnas_a_mantener):
-        df_filtrado = df[columnas_a_mantener]
+        df.columns = cols_tt
+
+        df_filtrado = df.loc[:, columnas_a_mantener]
+
         st.write(df_filtrado)
         
         # Crear un PDF a partir del DataFrame filtrado
